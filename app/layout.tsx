28bd0b14723a177cd5589cb4859d29123c63c99a
dkeum <dkeum@sfu.ca>
2024-01-29 13:31:15 -0800

    adding project

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "Posture Plus",
+  description: "Posture Plus Resources",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
+    </html>
+  );
+}
